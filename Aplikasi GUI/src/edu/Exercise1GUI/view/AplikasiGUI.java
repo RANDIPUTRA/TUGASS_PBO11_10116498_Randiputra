2785df1cff19608c0dc41bdc44cd718aadd13e3f
RANDIPUTRA <32483773+RANDIPUTRA@users.noreply.github.com>
2017-12-20 02:04:21 +0700

    Add files via upload

---
+++ Aplikasi GUI/src/edu/Exercise1GUI/view/AplikasiGUI.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.Exercise1GUI.view;
+
+/**
+ *
+ * @author win 10
+ */
+public class AplikasiGUI {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
